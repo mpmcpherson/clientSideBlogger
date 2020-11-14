c42c48de850dd00177ed9b11304127f20e30e0af
Michael McPherson <michael@mpmcpherson.com>
2020-11-14 00:43:36 -0500

    Compiled TinyMCE and pulled it into a major DIV on the screen. Just like that I cooked up a blogging platform

--- uiSite/index.php
+++ uiSite/index.php
@@ -5,6 +5,7 @@
 <title>Add your title here</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no" />
 <link rel="stylesheet" href="css\style.css">
+<script type="text/javascript" src="js\tinymce.min.js"></script>
 
 </head>
 
@@ -15,37 +16,40 @@
 
 	<div class="wrapper ninety" id="container">
 
-		<div class="leftBar ninety" id="leftBar">
+		<div class="leftBar" id="leftBar">
 			left bar
 		</div>
 
-		<div class="mainBody ninety" id="body">
-			<div class="header">
-				<h3>Page Title</h3>
-			</div> 
-			<!--<button id="btn">Top Button Text</button>-->
-			<div id="text">Here's some text, and isn't it interesting?</div>
-			<div id="text">Lorem ipsum dolor sit amet. Dulche et decorum est</div>
-			<div id="text">Here's some text, and isn't it interesting?</div>
-			<div> End Text		</div>	
+		<div class="mainBody full" id="thisThing">
+			<div class="full" id="body"> <!--<button id="btn">Top Button Text</button>-->
+				<div id="text">Here's some text, and isn't it interesting?</div>
+				<div id="text">Lorem ipsum dolor sit amet. Dulche et decorum est</div>
+				<div id="text">Here's some text, and isn't it interesting?</div>
+				<div> End Text		</div>	
+			</div>
 		</div>
 
 	</div>
 </body>	
 </html>
 
+
 <script type="text/javascript" src="js\helper.js"></script>
+
 <script type="text/javascript">
-	var button = document.getElementById('btn');
-	var resultDiv = document.getElementById('results');
+	//var button = document.getElementById('btn');
+	var resultDiv = document.	getElementById('results');
 
 	docReady(function() {
     	let leftbar = document.getElementById('leftBar');
     	leftbar.setAttribute("class","leftBar full")
+		tinymce.init({
+		    selector: '#body'
+		});
 	});
 
  
-	button.addEventListener('click', function() {
+	/*button.addEventListener('click', function() {
 		//console.log("hit click");
 		alert("click");
   		/*getRequest('basicGenerator.php',
@@ -57,8 +61,8 @@
   				resultDiv.innerHTML = 'An error occurred during your request: ' +  response.status + ' ' + response.statusText;
   			},
   			null);
-*/
-	});
+
+	});*/
 
 
 
