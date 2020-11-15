e0f2d8e116144eb917a4a1f2c6a582b5d0497a48
Michael McPherson <michael@mpmcpherson.com>
2020-11-14 23:53:46 -0500

    finally working

--- bloggingClientBase/js/helper.js
+++ bloggingClientBase/js/helper.js
@@ -80,7 +80,6 @@
 	    }
 
 		xhr.open("POST", url, true);
-		//xhr.setRequestHeader('Content-Type', 'application/json');
 		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 	    if (!xhr) return false;
@@ -93,18 +92,6 @@
 	                success(xhr.responseText) : error(xhr.status);
 	        }
 	    };
-	    //let res = btoa(data);
-	    let breakout = JSON.stringify({
-		    //res
-		});
-		//console.log(res);
-		//console.log(atob(res));
-		//breakout = breakout.replace(/&/g, "myCustomAmp");
-		//breakout = breakout.replace(/=/g, "myCustomEq");
-		//breakout = breakout.replace(/"data":/g, "");
-		//console.log("value="+breakout);
-		//console.log(breakout);
-		//xhr.send("value="+breakout);
 		xhr.send(data);
 		return xhr;
 	}
