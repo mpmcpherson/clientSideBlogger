24823b5c526d0b454568991f506a09dc45692430
Michael McPherson <michael@mpmcpherson.com>
2020-11-14 22:22:43 -0500

    in the process of screwing things up more

--- bloggingClientBase/resources/postHandler.php
+++ bloggingClientBase/resources/postHandler.php
@@ -3,8 +3,9 @@
 //print_r($_GET, TRUE)
 //file_put_contents('php://stderr', var_dump($_GET));
 
-echo var_dump($_POST);
-
+//echo $_POST["value"];
+echo var_dump($HTTP_RAW_POST_DATA);
 //file_put_contents("testData.txt", file_get_contents('php://input'));
+//echo $_POST["value"];
 
 ?>
