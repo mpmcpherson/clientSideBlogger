9988a4c8340e5028b4fde4f2bd6ac0283475a39d
Michael McPherson <michael@mpmcpherson.com>
2020-11-16 14:15:47 -0500

    validating that this works in the wild and identifying some future issues to solve

--- bloggingClientBase/resources/postHandler.php
+++ bloggingClientBase/resources/postHandler.php
@@ -3,7 +3,7 @@
 //looks like I can save it into the DB and then get it back out unchanged. Beautiful.
 
 
-file_put_contents("../posts/".date("Y-m-d H:i:s"), htmlspecialchars(file_get_contents('php://input')));
+file_put_contents("../frontEnd/posts/".date("Y-m-d H:i:s"), htmlspecialchars(file_get_contents('php://input')));
 
 //file_get_contents('ssh2.sftp://user:pass@example.com:22/path/to/filename');
 
