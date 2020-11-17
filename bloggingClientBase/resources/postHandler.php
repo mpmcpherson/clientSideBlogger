3f3c302b6b766f2eaba9406ce8e075a27a04d970
Michael McPherson <michael@mpmcpherson.com>
2020-11-16 23:37:16 -0500

    pathing out sftp stuff

--- bloggingClientBase/resources/postHandler.php
+++ bloggingClientBase/resources/postHandler.php
@@ -1,10 +1,13 @@
 <?php
 
 //looks like I can save it into the DB and then get it back out unchanged. Beautiful.
-
+$user = "";
+$pass = "";
+$server = "";
+$path = "";
 
 file_put_contents("../frontEnd/posts/".date("Y-m-d H:i:s"), htmlspecialchars(file_get_contents('php://input')));
 
-//file_get_contents('ssh2.sftp://user:pass@example.com:22/path/to/filename');
+file_put_contents("ssh2.sftp://" . $user . ":" . $pass . "@" . $server . ":22" . $path);
 
 ?>
