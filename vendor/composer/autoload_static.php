33ad9401749b61f2927d576ebec4c689fa67bb5e
Michael McPherson <michael@mpmcpherson.com>
2020-12-21 14:01:22 -0500

    straightening out lingering branch merge issues

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,40 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit0bca5e5058bf356f14f52ae1d26add17
 {
+    public static $prefixLengthsPsr4 = array (
+        'r' => 
+        array (
+            'rest\\api\\' => 9,
+        ),
+        'L' => 
+        array (
+            'League\\Plates\\' => 14,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'rest\\api\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/michaelmcpherson/php-insecure-rest/api/config',
+            1 => __DIR__ . '/..' . '/michaelmcpherson/php-insecure-rest/api/objects',
+            2 => __DIR__ . '/..' . '/michaelmcpherson/php-insecure-rest/api/shared',
+        ),
+        'League\\Plates\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/league/plates/src',
+        ),
+    );
+
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit0bca5e5058bf356f14f52ae1d26add17::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit0bca5e5058bf356f14f52ae1d26add17::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit0bca5e5058bf356f14f52ae1d26add17::$classMap;
 
         }, null, ClassLoader::class);
     }
